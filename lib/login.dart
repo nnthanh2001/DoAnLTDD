d8d3d3561fd38d2f27e0ee239ea122d0959a2000
nnthanh2001 <0306191365@caothang.edu.vn>
2022-06-11 08:12:10 +0700

    login

---
+++ lib/login.dart
@@ -0,0 +1,77 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('asset/image/user.png'),
+      ),
+    );
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Enter your email...',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Enter your password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final loginButton = new RaisedButton(
+      child: const Text('Sing in'),
+      textColor: Colors.white,
+      color: Theme.of(context).accentColor,
+      elevation: 10.0,
+      splashColor: Colors.blueGrey,
+      onPressed: () {},
+    );
+    final forgotLabel = FlatButton(
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {},
+    );
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            children: <Widget>[
+              logo,
+              SizedBox(height: 45.0),
+              email,
+              SizedBox(height: 10.0),
+              password,
+              SizedBox(height: 15.0),
+              loginButton,
+              forgotLabel
+            ],
+          ),
+        ));
+  }
+}
